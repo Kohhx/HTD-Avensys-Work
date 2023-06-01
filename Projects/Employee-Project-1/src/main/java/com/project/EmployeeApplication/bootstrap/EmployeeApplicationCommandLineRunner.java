735985f732ad9eed0595b61417e7c1e401fc37b4
He Xiang Koh <kohhxx6@gmail.com>
2023-06-02 01:35:11 +0800

    added commandline runner and test dataJPA and service. Working

--- Projects/Employee-Project-1/src/main/java/com/project/EmployeeApplication/bootstrap/EmployeeApplicationCommandLineRunner.java
+++ Projects/Employee-Project-1/src/main/java/com/project/EmployeeApplication/bootstrap/EmployeeApplicationCommandLineRunner.java
@@ -1,8 +1,14 @@
 package com.project.EmployeeApplication.bootstrap;
 
+import com.project.EmployeeApplication.employee.Employee;
 import com.project.EmployeeApplication.employee.EmployeeRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class EmployeeApplicationCommandLineRunner implements CommandLineRunner {
 
     private final EmployeeRepository repository;
@@ -13,7 +19,10 @@ public class EmployeeApplicationCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        
-
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("He Xiang", "Koh", "koh@gmail.com", 33, "Bishan st 24"));
+        employees.add(new Employee("Leon", "Boon", "leon33@gmail.com", 26, "Woodlands st 12"));
+        employees.add(new Employee("Ashley", "Yeo", "ashley@gmail.com", 35, "Tampines st 21"));
+        repository.saveAll(employees);
     }
 }
