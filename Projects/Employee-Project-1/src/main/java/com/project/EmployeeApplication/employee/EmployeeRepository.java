a259c1d90a47d020fdc1b0165906571cd417aec4
He Xiang Koh <kohhxx6@gmail.com>
2023-06-02 01:11:22 +0800

    Added employee jpa repository

--- Projects/Employee-Project-1/src/main/java/com/project/EmployeeApplication/employee/EmployeeRepository.java
+++ Projects/Employee-Project-1/src/main/java/com/project/EmployeeApplication/employee/EmployeeRepository.java
@@ -1,4 +1,6 @@
 package com.project.EmployeeApplication.employee;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
