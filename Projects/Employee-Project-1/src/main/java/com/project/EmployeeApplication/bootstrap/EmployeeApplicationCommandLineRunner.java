cf0271797968b2880840530ac2c3fd088b7ccb30
He Xiang Koh <kohhxx6@gmail.com>
2023-06-02 01:05:10 +0800

    Setup H2-database and configuration

---
+++ Projects/Employee-Project-1/src/main/java/com/project/EmployeeApplication/bootstrap/EmployeeApplicationCommandLineRunner.java
@@ -0,0 +1,10 @@
+package com.project.EmployeeApplication.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class EmployeeApplicationCommandLineRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
